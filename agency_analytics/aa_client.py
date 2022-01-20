144ea83e02ff09b23e574de4a4a85847a2d79f3f
rheophile10 <rheophile10@gmail.com>
2022-01-20 01:37:38 -0700

    added keyword rankings methods to aa_client and tests

--- agency_analytics/aa_client.py
+++ agency_analytics/aa_client.py
@@ -51,6 +51,18 @@ class Client:
         self._list_data('keywords', params)
         return self.keywords 
 
+    def get_keyword_rankings_list(self, campaign_id, start_date, end_date, search=None, page=None, limit=None, 
+        sort_metric=None, sort_direction=None, compare_previous_method=None):
+        """list all keyword rankings https://agencyanalytics.com/docs/api/feeds#list-campaign-rankings-by-keyword"""
+        start_date = start_date.strftime(start_date, '%Y-%m-%d')
+        end_date = end_date.strftime(end_date, '%Y-%m-%d')
+        args = {'campaign_id':campaign_id, 'start_date':start_date, 'end_date':end_date, 
+            'search':search, 'page':page, 'limit':limit, 'sort_metric':sort_metric, 
+            'sort_direction':sort_direction, 'compare_previous_method':compare_previous_method}
+        params = self._build_params(args)
+        self._list_data('keyword_rankings', params)
+        return self.rankings
+
     def _write_list_data(self, endpoint, write_function, params=None):
         """operates the list api endpoints and executes supplied write function"""
         pages, data = self._make_request(endpoint, params)
@@ -64,15 +76,24 @@ class Client:
             write_function(data)
 
     def write_campaigns_list(self, write_function, filter=None, page=None, limit=None, order_by=None, order_direction=None):
-        """list all campaigns https://agencyanalytics.com/docs/api/campaigns#list-all-campaigns"""
+        """write all campaigns https://agencyanalytics.com/docs/api/campaigns#list-all-campaigns"""
         args = {'filter': filter, 'page':page, 'limit':limit, 'order_by': order_by, 'order_direction':order_direction}
         params = self._build_params(args)
         self._write_list_data('campaigns', write_function, params)
-        return self.campaigns 
 
     def write_keywords_list(self, write_function, filter=None, page=None, limit=None, order_by=None, order_direction=None):
-        """list all keywords https://agencyanalytics.com/docs/api/keywords#list-all-keywords"""
+        """write all keywords https://agencyanalytics.com/docs/api/keywords#list-all-keywords"""
         args = {'filter': filter, 'page':page, 'limit':limit, 'order_by': order_by, 'order_direction':order_direction}
         params = self._build_params(args)
         self._write_list_data('keywords', write_function, params)
-        return self.keywords 
+    
+    def write_keyword_rankings_list(self, write_function, campaign_id, start_date, end_date, search=None, page=None, limit=None, 
+        sort_metric=None, sort_direction=None, compare_previous_method=None):
+        """write all keyword rankings https://agencyanalytics.com/docs/api/feeds#list-campaign-rankings-by-keyword"""
+        start_date = start_date.strftime(start_date, '%Y-%m-%d')
+        end_date = end_date.strftime(end_date, '%Y-%m-%d')
+        args = {'campaign_id':campaign_id, 'start_date':start_date, 'end_date':end_date, 
+            'search':search, 'page':page, 'limit':limit, 'sort_metric':sort_metric, 
+            'sort_direction':sort_direction, 'compare_previous_method':compare_previous_method}
+        params = self._build_params(args)
+        self._list_data('keyword_rankings', write_function, params)
--- agency_analytics/tests/agency_analytics_test.py
+++ agency_analytics/tests/agency_analytics_test.py
@@ -1,6 +1,7 @@
 import pytest
 from dotenv import dotenv_values
 from agency_analytics.aa_client import Client
+from datetime import datetime, timedelta
 
 class Writer:
     def __init__(self):
@@ -22,6 +23,22 @@ def test_get_keywords_list(aa_client):
     aa_client.get_keywords_list(limit=5)
     assert len(aa_client.keywords) == 5     
 
+@pytest.fixture
+def keyword_rankings_args(aa_client):
+    aa_client.get_campaigns_list(limit=5)
+    campaign_id = aa_client.campaigns[0]['id']
+    date_created = datetime.strptime(aa_client.campaigns[0]['date_created'], '%Y-%m-%d %H:%M:%S')
+    start_date = date_created - timedelta(days = 5)
+    end_date = datetime.today()
+    return campaign_id, start_date, end_date
+
+def test_get_keyword_rankings_list(aa_client, keyword_rankings_args):
+    aa_client.get_campaigns_list(limit=5)
+    campaign_id, start_date, end_date = keyword_rankings_args
+    aa_client.get_keyword_rankings_list(campaign=campaign_id, start_date=start_date, 
+        end_date=end_date, limit=5)
+    assert len(aa_client.keyword_rankings) > 0     
+
 @pytest.fixture 
 def writer():
     return Writer()   
@@ -33,3 +50,9 @@ def test_write_campaigns_list(aa_client, writer):
 def test_write_keywords_list(aa_client, writer):
     aa_client.write_keywords_list(writer.write, limit=5)
     assert len(writer.data) == 5
+
+def test_write_keyword_rankings_list(aa_client, writer, keyword_rankings_args):
+    campaign_id, start_date, end_date = keyword_rankings_args
+    aa_client.get_keyword_rankings_list(write_function = writer.write, campaign=campaign_id, 
+        start_date=start_date, end_date=end_date, limit=5)
+    assert len(writer.data) > 0

2282e9a6322b5a0433ee9994bfff947f8df148de
rheophile10 <rheophile10@gmail.com>
2022-01-22 12:40:08 -0700

    changed dayspan

--- pubsub_main.py
+++ pubsub_main.py
@@ -5,5 +5,5 @@ def get_keywords(event, context):
      """
      wipe_yesterday_data()
      campaigns_list = []
-     call_gcloud_fxn(campaigns_list, 390)
+     call_gcloud_fxn(campaigns_list, 365)
 

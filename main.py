bb00e708e62248f67e7404bee960b3b7eb1f6dc8
rheophile10 <rheophile10@gmail.com>
2022-01-22 17:22:51 -0700

    write keywords needed to return something

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ import json
 def write_keywords(campaign, day_span, aa_client, bigquery_client):
     end_date = datetime.today()
     start_date = end_date - timedelta(days = day_span)
-    aa_client.write_keyword_rankings_list(bigquery_client.insert_keyword_rankings,campaign, start_date, end_date)
+    return aa_client.write_keyword_rankings_list(bigquery_client.insert_keyword_rankings,campaign, start_date, end_date)
+    
 
 async def call_gcloud_fxn(campaigns_list, day_span):
     new_request_json = json.dumps({'campaigns_list': campaigns_list, 'day_span': day_span})

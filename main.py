fb3e4cd0df3d2844aeef02e94f9931903fcf7b5f
rheophile10 <rheophile10@gmail.com>
2022-01-24 14:59:52 -0700

    pubsub main

--- main.py
+++ main.py
@@ -1,58 +1,66 @@
-import asyncio
 from bigquery_writer.bigclient import OurClient
 #from dotenv import dotenv_values
 from agency_analytics.aa_client import Client
 from datetime import datetime, timedelta
-import requests
 import os
-import google.auth.transport.requests
-import google.oauth2.id_token
 import json
 from google.cloud import pubsub_v1
+import base64
 
 def write_keywords(campaign, day_span, aa_client, bigquery_client):
     end_date = datetime.today()
     start_date = end_date - timedelta(days = day_span)
     return aa_client.write_keyword_rankings_list(bigquery_client.insert_keyword_rankings,campaign, start_date, end_date)
     
-async def call_gcloud_fxn(campaigns_list, day_span):
-    new_request_json = json.dumps({'campaigns_list': campaigns_list, 'day_span': day_span})
-    url = os.getenv('FXN_URL')
-    auth_req = google.auth.transport.requests.Request()
-    id_token = google.oauth2.id_token.fetch_id_token(auth_req, url)
-    headers = {'Authorization': f'Bearer {id_token}', "Content-Type": "application/json"}
-    response = requests.post(url, data=new_request_json, headers=headers)
-    await response.status_code
-
 def wipe_yesterday_data():
     yesterday = datetime.today() - timedelta(days = 1)
     yesterday = yesterday.strftime('%Y-%m-%d')
     bigquery = OurClient(os.getenv('PROJECT_ID'), os.getenv('DATASET'))
     bigquery.delete_keyword_rankings_with_date(yesterday)
 
-def get_keywords(request):
-    """Updates keywords table in bigquery
+def publish_campaign(campaign):
+    publisher = pubsub_v1.PublisherClient()
+    PROJECT_ID = os.getenv('PROJECT_ID')
+    campaign_json = json.dumps({
+        'data': campaign,
+    })
+    campaign_bytes = campaign_json.encode('utf-8')
+    TOPIC_NAME = os.getenv('TOPIC_NAME')
+    topic_path = publisher.topic_path(PROJECT_ID, TOPIC_NAME)
+    try:
+        publish_future = publisher.publish(topic_path, data=campaign_bytes)
+        publish_future.result()  # Verify the publish succeeded
+        return 'Message published.'
+    except Exception as e:
+        return e
+
+def isint(some_string):
+    try: 
+        int(some_string)
+    except ValueError:
+        return False
+    else:
+        return True
+
+def get_keywords(event, context):
+    """Triggered from a message on a Cloud Pub/Sub topic.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
     """
-    request_json = request.get_json()
-    campaigns_list = request_json['campaigns_list']
-    day_span = request_json['day_span']
+    data = base64.b64decode(event['data']).decode('utf-8')
     bigquery = OurClient(os.getenv('PROJECT_ID'), os.getenv('DATASET'))
     aa = Client(os.getenv('KEY'))
-    if campaigns_list == []:
+    day_span = 365
+    if data == 'start':
         wipe_yesterday_data()
         campaigns_list = aa.get_campaigns_list(active_only=True)
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        tasks = [loop.create_task(call_gcloud_fxn([campaign], day_span)) for campaign in campaigns_list]
-        loop.run_until_complete(asyncio.wait(tasks))
-        loop.close()
-        return f'finished: campaigns_list length {len(campaigns_list)}'
-    elif len(campaigns_list) == 1: 
-        campaign = campaigns_list[0]
-        return write_keywords(campaign, day_span, aa, bigquery)
-    else:
-        return 'unexpected error'
-
+        for campaign in campaigns_list:
+            publish_campaign(campaign)
+    elif isint(data):
+        write_keywords(data, day_span, aa, bigquery)
+    else: 
+        return f'Unexpected Error. data: {data}'
 
 
 
